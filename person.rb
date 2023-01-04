4b485f73ec03c334dc04b06d1918d569f560026c
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2023-01-04 21:14:06 +0600

    Update code for associations

--- person.rb
+++ person.rb
@@ -1,6 +1,7 @@
 require './nameable'
 require './trimmer'
 require './capitalize'
+require './rental'
 
 class Person < Nameable
   attr_accessor :name, :age
@@ -11,6 +12,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
     super()
   end
 
@@ -22,16 +24,13 @@ class Person < Nameable
     @name
   end
 
+  def add_rental(date, person)
+    Rental.new (date, self, person)
+  end
+
   private
 
   def of_age?
     return true if @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
--- student.rb
+++ student.rb
@@ -1,8 +1,15 @@
 require_relative './person'
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     @classroom = classroom
     super(name, age, parent_permission)
+  end
+
+  def add_classroom
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
